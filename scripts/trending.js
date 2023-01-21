f71938310fd1489144d73dd0808d1b6db1d80bf5
MoovendhanR <moovendhanr1011@gmail.com>
2023-01-21 16:46:06 +0530

    netflix trending part appended successfully

--- scripts/trending.js
+++ scripts/trending.js
@@ -11,3 +11,34 @@ async function loadTrending(){
 }
 
 loadTrending()
+
+function showTrending(results){
+    document.querySelector("#trending-container").textContent = "";
+    results.forEach(function(trending) {
+        var outerDiv = document.createElement("div");
+        var img = document.createElement("img");
+        img.src = "https://image.tmdb.org/t/p/original/" + trending.backdrop_path;
+        var innerDiv = document.createElement("div");
+        var h5 = document.createElement("h5");
+        h5.textContent = trending.media_type;
+        var h2 = document.createElement("h2");
+        if(trending.name){
+            h2.textContent = trending.name;
+        }
+        else{
+            h2.textContent = trending.original_title
+        }
+        var h6 = document.createElement("div");
+        if(trending.vote_count == 0){
+            h6.textContent = "N/A";
+        }
+        else{
+            h6.textContent = trending.vote_average  + " from " + trending.vote_count + " votes";
+        }
+        var p = document.createElement("p");
+        p.textContent = trending.overview;
+        innerDiv.append(h5, h2, h6, p);
+        outerDiv.append(img, innerDiv);
+        document.querySelector("#trending-container").append(outerDiv);
+    });
+}
